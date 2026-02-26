413d0f328376e38949bb6fc0661d199216e579b3
jinnturtle <egined@gmail.com>
2026-02-26 16:25:25 +0000

    First load

---
+++ org/.dir-locals.el
@@ -0,0 +1,7 @@
+((nil
+  . ((org-publish-project-alist
+      . (("homepage"
+          :base-directory "."
+          :publishing-directory "../html"
+          :recursive t
+          :exclude "head\\.org"))))))
